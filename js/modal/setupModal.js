064670fa3589920cf7f69322a93109eda78f3e83
Vinicius Barbosa <viniciusds.barbosa@hotmail.com>
2021-04-29 17:51:08 -0300

    adicionando site LIVROFIX

---
+++ js/modal/setupModal.js
@@ -0,0 +1,14 @@
+$('a[href="#ex8"]').click(function(event) {
+  event.preventDefault();
+  $(this).modal({
+    fadeDuration: 250,
+    
+  });
+});
+$('a[href="#ex9"]').click(function(event) {
+  event.preventDefault();
+  $(this).modal({
+    fadeDuration: 250,
+    
+  });
+});
